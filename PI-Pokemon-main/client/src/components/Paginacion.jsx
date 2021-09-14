d2d2eb94eee77c63782731c6ecabbb5b519457cd
tomasgoyret <tomas.goyret@gmail.com>
2021-09-14 01:30:19 -0300

    casiFinal

--- PI-Pokemon-main/client/src/components/Paginacion.jsx
+++ PI-Pokemon-main/client/src/components/Paginacion.jsx
@@ -5,16 +5,16 @@ export function Paginacion({pokesPorPage, totalPokes, pagina}) {
     const numerosPag = [];
     if(totalPokes<=3){
         return <div></div>
+    } else {
+        for(let i=1; i <= Math.ceil( totalPokes / pokesPorPage ) ; i++){
+            numerosPag.push(i)
+        }
+        return <nav>
+            <ul className="Paginacion">
+                 {numerosPag.map((number) =>{
+                     return <button key={number} className="itemPaginacion" onClick={()=> pagina(number)}> {number} </button>
+                 })}   
+            </ul>
+        </nav>
     }
-
-    for(let i=1; i <= Math.ceil( totalPokes / pokesPorPage ) ; i++){
-        numerosPag.push(i)
     }
-    return <nav>
-        <ul className="Paginacion">
-             {numerosPag.map((number) =>{
-                 return <button key={number} className="itemPaginacion" onClick={()=> pagina(number)}> {number} </button>
-             })}   
-        </ul>
-    </nav>
-}
--- PI-Pokemon-main/client/src/components/PokeCard.jsx
+++ PI-Pokemon-main/client/src/components/PokeCard.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './styles.css/PokeCard.css'
+import Pikachu from './styles.css/media/ohNoPikachu.png'
 
 export function PokeCard({ loading, pokemons }) {
     if (loading) {
         return <h2>Loading... </h2>
     }
-    if(pokemons[0].message){
-        return <h2>{pokemons[0].message}</h2>
+    console.log(pokemons)
+    if(!pokemons.length){
+        return <h2 className='mensajeErr'> <img className="pikachuDet" src={Pikachu} alt="ohno!" />   No hay resultados </h2>
     }
+    if(pokemons[0].message){
+        return <h2 className='mensajeErr'><img className="pikachuDet" src={Pikachu} alt="ohno!" />       {pokemons[0].message}</h2>
+    }
+    
     return <div className="PokeCard">
         {pokemons && pokemons.map((pokemon) => {
             return <div className='PokeCard_Item' key={pokemon.id}>
@@ -18,7 +24,7 @@ export function PokeCard({ loading, pokemons }) {
                     </Link>
                 </p>
                 <img className='imgPokeCard' src={pokemon.image} alt="imagen" />
-                <p>Tipos: {pokemon.tipo.map((p) => <p key={p}> {p} </p>)}</p>
+                <p>Tipos: {pokemon.tipo.map((p) => <li key={p}> {p} </li>)}</p>
                 <p className='attack'>Attack : {pokemon.attack}</p>
             </div>
         } 
